beb5237a186d1569a5e7e75e4e374750917bc716
Marcelo Choque <pablo_marce@hotmail.com>
2018-05-27 23:12:25 -0400

    finalizado con edicion de todo

--- Proyectos/todolistVue/src/app.js
+++ Proyectos/todolistVue/src/app.js
@@ -2,25 +2,43 @@ var app = new Vue({
     el: '#app',
     data: {
         todo: null,
-        todos: []
+        todos: [],
+        editTodo: null
     },
     methods: {
         add(todo) {
             if (!this.todoIsValid) {
                 return;
             }
-            this.todos.push({ content: todo, finished: false });
+            this.todos.push({ content: todo, finished: false, isEdited: false });
             this.todo = null;
         },
         remove(todo) {
             this.todos = this.todos.filter((item) => {
                 return item != todo;
             });
+        },
+        edit(todo) {
+            this.editTodo = todo.content;
+            todo.isEdited = true;
+        },
+        update(todo) {
+            if (!this.editTodoIsValid) {
+                return;
+            }
+            todo.content = this.editTodo;
+            todo.isEdited = false;
         }
     },
     computed: {
         todoIsValid() {
             return !!this.todo;
+        },
+        editTodoIsValid() {
+            return !!this.editTodo;
+        },
+        isBeingEdited() {
+            return this.todos.filter(todo => todo.isEdited).length > 0;
         }
     }
 })
